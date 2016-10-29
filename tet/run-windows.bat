265f4de8857f2c0e3e2e4b6dc23192ef3389de1e
Jer Wilson <realaddress@legiturl.com>
2016-10-28 23:47:49 -0700

    Finishing combining into tinycgames

--- tet/run-windows.bat
+++ tet/run-windows.bat
@@ -1,8 +1,12 @@
 @echo off
 setlocal
-set PATH=%PATH%;..\SDL2-2.0.5\lib\x64
-set PATH=%PAtH%;..\SDL2_ttf-2.0.14\x86_64-w64-mingw32\lib
-set PATH=%PAtH%;..\SDL2_ttf-2.0.14\x86_64-w64-mingw32\bin
 
-..\tcc\tcc.exe -DSDL_MAIN_HANDLED -I..\SDL2-2.0.5\include -L..\SDL2-2.0.5\lib\x64 -lSDL2 -run tet.c
+set CC=..\win\tcc\tcc.exe
+set SDL=..\win\SDL2-2.0.5
+set TTF=..\win\SDL2_ttf-2.0.14\x86_64-w64-mingw32
 
+set PATH=%PATH%;%SDL%\lib\x64
+set PATH=%PAtH%;%TTF%\lib
+set PATH=%PAtH%;%TTF%\bin
+
+%CC% -DSDL_MAIN_HANDLED -I%SDL%\include -I%TTF%/include/SDL2 -L%SDL%\bin\x64 -L%TTF%/bin -lSDL2 -lSDL2_ttf -run tet.c
