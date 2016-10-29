4b3d302d64efc998dd2bb4cfd656f57acaec123a
Jer Wilson <jwilson@classifiedads.com>
2016-10-28 19:25:52 -0700

    cleanup

--- flappy.c
+++ flappy.c
@@ -11,26 +11,24 @@
 #define PHYS_W (W + PIPE_W + 80)
 #define GAP 220
 #define GRACE 4
+#define RANDOM_PIPE_HEIGHT (rand() % (H - GROUND - GAP - 120) + 60)
+#define PLYR_X 80
+#define PLYR_SZ 60
 
-enum gamestates {READY, ALIVE, GAMEOVER};
+enum gamestates {READY, ALIVE, GAMEOVER} gamestate = READY;
 
-int gamestate = READY;
-float player_x = 80;
 float player_y = (H - GROUND)/2;
-float player_w = 60;
-float player_h = 60;
 float player_vel;
-
 int pipe_x[2] = {W, W};
 float pipe_y[2];
 int score;
 int best;
-
-int idle_time;
+int idle_time = 30;
 float frame = 0;
 
 SDL_Event event;
 SDL_Renderer *renderer;
+SDL_Surface *surf;
 SDL_Texture *pillar;
 SDL_Texture *background;
 SDL_Texture *bird[4];
@@ -38,12 +36,10 @@ TTF_Font *font;
 
 void setup();
 void new_game();
-void key_down();
 void update_stuff();
 void update_pipe(int i);
-int random_pipe_height();
 void draw_stuff();
-void text(char *msg, int y);
+void text(char *fstr, int value, int height);
 
 //the entry point and main game loop
 int main()
@@ -54,9 +50,19 @@ int main()
         {
                 while(SDL_PollEvent(&event)) switch(event.type)
                 {
-                        case SDL_QUIT: exit(0);
-                        case SDL_KEYDOWN: key_down(); break;
-                        case SDL_MOUSEBUTTONDOWN: key_down(); break;
+                        case SDL_QUIT:
+                                exit(0);
+                        case SDL_KEYDOWN:
+                        case SDL_MOUSEBUTTONDOWN:
+                                if(gamestate == ALIVE)
+                                {
+                                        player_vel = -11.7f;
+                                        frame += 1.0f;
+                                }
+                                else if(idle_time > 30)
+                                {
+                                        new_game();
+                                }
                 }
 
                 update_stuff();
@@ -72,28 +78,14 @@ void setup()
         srand(time(NULL));
 
         SDL_Init(SDL_INIT_VIDEO);
-
         SDL_Window *win = SDL_CreateWindow("Flappy",
-                        SDL_WINDOWPOS_UNDEFINED,
-                        SDL_WINDOWPOS_UNDEFINED,
-                        W,
-                        H,
-                        SDL_WINDOW_SHOWN);
-
+                SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, W, H, SDL_WINDOW_SHOWN);
         renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_PRESENTVSYNC);
-
-        if(!renderer)
-        {
-                fprintf(stderr, "Could not create SDL renderer for some reason\n");
-                exit(-1);
-        }
-
-        SDL_Surface *surf;
+        if(!renderer) exit(fprintf(stderr, "Could not create SDL renderer\n"));
 
         surf = SDL_LoadBMP("res/pillar.bmp");
         SDL_SetColorKey(surf, 1, 0xffff00);
         pillar = SDL_CreateTextureFromSurface(renderer, surf);
-
         surf = SDL_LoadBMP("res/background.bmp");
         background = SDL_CreateTextureFromSurface(renderer, surf);
 
@@ -107,22 +99,20 @@ void setup()
         }
 
         TTF_Init();
-        font = TTF_OpenFont("res/LiberationMono-Regular.ttf", 50);
-        printf("%p %s\n", font, SDL_GetError());
+        font = TTF_OpenFont("res/LiberationMono-Regular.ttf", 42);
 }
 
 //start a new game
 void new_game()
 {
         gamestate = ALIVE;
-        player_x = 80;
         player_y = (H - GROUND)/2;
         player_vel = -11.7f;
         score = 0;
         pipe_x[0] = PHYS_W + PHYS_W/2 - PIPE_W;
         pipe_x[1] = PHYS_W - PIPE_W;
-        pipe_y[0] = random_pipe_height();
-        pipe_y[1] = random_pipe_height();
+        pipe_y[0] = RANDOM_PIPE_HEIGHT;
+        pipe_y[1] = RANDOM_PIPE_HEIGHT;
 }
 
 //when we hit something
@@ -133,25 +123,10 @@ void game_over()
         if(best < score) best = score;
 }
 
-//handle a key press from the player
-void key_down()
-{
-        if(gamestate == ALIVE)
-        {
-                player_vel = -11.7f;
-                frame += 1.0f;
-        }
-        else if(idle_time > 30)
-        {
-                new_game();
-        }
-}
-
 //update everything that needs to update on its own, without input
 void update_stuff()
 {
-        if(gamestate != ALIVE)
-                return;
+        if(gamestate != ALIVE) return;
 
         player_y += player_vel;
         player_vel += 0.61; // gravity
@@ -159,43 +134,33 @@ void update_stuff()
         if(player_vel > 10.0f)
                 frame = 0;
         else
-                frame -= (player_vel - 10.0f) * 0.03f;
+                frame -= (player_vel - 10.0f) * 0.03f; //fancy animation
 
-        // hit ground?
-        if(player_y > H - GROUND - player_h)
+        if(player_y > H - GROUND - PLYR_SZ)
                 game_over();
 
-        // pipes
         for(int i = 0; i < 2; i++)
                 update_pipe(i);
 }
 
+//update one pipe for one frame
 void update_pipe(int i)
 {
-        // collide player
-        if(player_x + player_w >= pipe_x[i] + GRACE && player_x <= pipe_x[i] + PIPE_W - GRACE &&
-                (player_y <= pipe_y[i] - GRACE || player_y + player_h >= pipe_y[i] + GAP + GRACE))
-        {
-                game_over();
-                return;
-        }
+        if(PLYR_X + PLYR_SZ >= pipe_x[i] + GRACE && PLYR_X <= pipe_x[i] + PIPE_W - GRACE &&
+                (player_y <= pipe_y[i] - GRACE || player_y + PLYR_SZ >= pipe_y[i] + GAP + GRACE))
+                game_over(); // player hit pipe
 
-        // move pipes
+        // move pipes and increment score if we just passed one
         pipe_x[i] -= 5;
-
-        if(pipe_x[i] + PIPE_W < player_x && pipe_x[i] + PIPE_W > player_x - 5)
+        if(pipe_x[i] + PIPE_W < PLYR_X && pipe_x[i] + PIPE_W > PLYR_X - 5)
                 score++;
 
+        // respawn pipe once far enough off screen
         if(pipe_x[i] <= -PIPE_W)
         {
                 pipe_x[i] = PHYS_W - PIPE_W;
-                pipe_y[i] = random_pipe_height();
+                pipe_y[i] = RANDOM_PIPE_HEIGHT;
         }
-}
-
-int random_pipe_height()
-{
-        return rand() % (H - GROUND - GAP - 120) + 60;
 }
 
 //draw everything in the game on the screen
@@ -214,39 +179,27 @@ void draw_stuff()
         }
 
         //draw player
-        SDL_RenderCopy(renderer, bird[(int)frame % 4], NULL, &(SDL_Rect){player_x, player_y, player_w, player_h});
+        SDL_RenderCopy(renderer, bird[(int)frame % 4], NULL,
+                        &(SDL_Rect){PLYR_X, player_y, PLYR_SZ, PLYR_SZ});
 
-        //draw text
-        char msg[80];
-        if(gamestate != READY)
-        {
-                snprintf(msg, 80, "%d", score);
-                text(msg, 10);
-        }
-
-        if(gamestate != ALIVE)
-        {
-                text(gamestate == GAMEOVER ? "Game Over" : "Press any key", 150);
-                if(gamestate == GAMEOVER)
-                {
-                        snprintf(msg, 80, "Best: %d", best);
-                        text(msg, 250);
-                }
-        }
+        if(gamestate != READY) text("%d", score, 10);
+        if(gamestate == READY) text("Press any key", 0, 150);
+        if(gamestate == GAMEOVER) text("High score: %d", best, 150);
 
         SDL_RenderPresent(renderer);
 }
 
-void text(char *msg, int y)
+void text(char *fstr, int value, int height)
 {
         if(!font) return;
-
         int w, h;
+        char msg[80];
+        snprintf(msg, 80, fstr, value);
         TTF_SizeText(font, msg, &w, &h);
         SDL_Surface *msgsurf = TTF_RenderText_Blended(font, msg, (SDL_Color){255, 255, 255});
         SDL_Texture *msgtex = SDL_CreateTextureFromSurface(renderer, msgsurf);
         SDL_Rect fromrec = {0, 0, msgsurf->w, msgsurf->h};
-        SDL_Rect torec = {(W - w)/2, y, msgsurf->w, msgsurf->h};
+        SDL_Rect torec = {(W - w)/2, height, msgsurf->w, msgsurf->h};
         SDL_RenderCopy(renderer, msgtex, &fromrec, &torec);
         SDL_DestroyTexture(msgtex);
         SDL_FreeSurface(msgsurf);
