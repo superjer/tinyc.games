b567376beac578a2749e02a0ed174fe58a00a630
Jer Wilson <realaddress@legiturl.com>
2016-11-03 15:59:38 -0700

    Simplify tet.c a little

--- tet/tet.c
+++ tet/tet.c
@@ -4,62 +4,48 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 
-#define SHAPES 7
 #define BWIDTH 10  // board width, height
 #define BHEIGHT 20
-#define BS 30
-#define EXTRA_DEAD 50
+#define BS 30      // size of one block
+#define BS2 (BS/2) // size of half a block
+#define PREVIEW_BOX_X (10 + BS * BWIDTH + 10 + BS2)
+#define MAX(a,b) ((a)>(b)?(a):(b))
 
 char shapes[] =
         ".... .... .... .... .... .... .... .O.. "
         ".... .OO. .OO. .OO. ..O. .O.. .... .O.. "
         ".... .O.. ..O. .OO. .OO. .OO. OOO. .O.. "
         ".... .O.. ..O. .... .O.. ..O. .O.. .O.. "
-                                      
         ".... .... .... .... .... .... .... .... "
         ".... .O.. ..O. .OO. OO.. .OO. .O.. .... "
         ".... .OOO OOO. .OO. .OO. OO.. .OO. OOOO "
         ".... .... .... .... .... .... .O.. .... "
-
         ".... .... .... .... .... .... .... .O.. "
         ".... ..O. .O.. .OO. ..O. .O.. .O.. .O.. "
         ".... ..O. .O.. .OO. .OO. .OO. OOO. .O.. "
         ".... .OO. .OO. .... .O.. ..O. .... .O.. "
-                                      
         ".... .... .... .... .... .... .... .... "
         ".... .OOO OOO. .OO. OO.. .OO. .O.. .... "
         ".... ...O O... .OO. .OO. OO.. OO.. OOOO "
-        ".... .... .... .... .... .... .O.. .... "
-        ;
+        ".... .... .... .... .... .... .O.. .... ";
 
-unsigned char colors[][9*3] = {
-        {
-                25,   40,  35, // "black"
-                242, 245, 237, // J
-                255, 194,   0, // L
-                15,  127, 127, // square
-                255,  91,   0, // Z
-                184,   0,  40, // S
-                74,  192, 242, // T
-                132,   0,  46, // line
-                255, 255, 255, // shine
-        }, {
-                  0,   0,   0, // "black"
-                221,  30,  47, // J
-                235, 176,  53, // L
-                  6, 162, 203, // square
-                 33, 133,  89, // Z
-                162,  89,  33, // S
-                 33,   6, 177, // T
-                208, 198, 177, // line
-                255, 255, 200, // shine
-        }
+int center[] = { // helps center shapes in preview box
+         0,0, 0,0, 0,0, 0,1, 0,0, 0,0, 1,-1,1,1,
 };
 
-int palette = 0;
+unsigned char colors[] = {
+        0,     0,   0, // unused
+        242, 245, 237, // J-piece
+        255, 194,   0, // L-piece
+        15,  127, 127, // square
+        255,  91,   0, // Z
+        184,   0,  40, // S
+        74,  192, 242, // T
+        132,   0,  46, // line-piece
+        255, 255, 255, // shine color
+};
 
 unsigned char board[BHEIGHT][BWIDTH];
-
 int killy_lines[BHEIGHT];
 
 int falling_x;
@@ -70,7 +56,6 @@ int next_shape;
 int lines;
 int score;
 int best;
-
 int idle_time;
 int shine_time;
 int dead_time;
@@ -79,17 +64,14 @@ SDL_Event event;
 SDL_Renderer *renderer;
 TTF_Font *font;
 
-//enginey protos
+//prototypes
 void setup();
 void key_down();
 void update_stuff();
 void draw_stuff();
 void draw_square(int x, int y, int shape);
-void draw_board_square(int bx, int by, int shape);
-void set_shape_color(int shape, int shade);
+void set_color_from_shape(int shape, int shade);
 void text(char *fstr, int value, int x, int y);
-
-//gamey protos
 void new_game();
 void new_piece();
 void move(int dx, int dy);
@@ -97,7 +79,6 @@ int is_solid_part(int shape, int rot, int i, int j);
 int collide(int x, int y, int rot);
 void bake();
 void check_lines();
-void check_dead();
 void shine_line(int y);
 void kill_lines();
 void slam();
@@ -128,7 +109,6 @@ int main()
 void setup()
 {
         srand(time(NULL));
-
         SDL_Init(SDL_INIT_VIDEO);
 
         SDL_Window *win = SDL_CreateWindow("Tet",
@@ -139,7 +119,6 @@ void setup()
                         SDL_WINDOW_SHOWN);
 
         renderer = SDL_CreateRenderer(win, -1, 0);
-
         if(!renderer)
         {
                 fprintf(stderr, "Could not create SDL renderer for some reason\n");
@@ -161,7 +140,6 @@ void key_down()
                 case SDLK_s: case SDLK_DOWN:  move( 0, 1); break;
                 case SDLK_q: case SDLK_z:     spin(-1);    break;
                 case SDLK_e: case SDLK_x:     spin( 1);    break;
-                case SDLK_SPACE:              palette = palette ? 0 : 1; break;
         }
 }
 
@@ -185,15 +163,13 @@ void update_stuff()
 
         if(dead_time > 0)
         {
-                int x = (dead_time-EXTRA_DEAD) % BWIDTH;
-                int y = (dead_time-EXTRA_DEAD) / BWIDTH;
+                int x = (dead_time) % BWIDTH;
+                int y = (dead_time) / BWIDTH;
 
                 if(y >= 0 && y < BHEIGHT && x >= 0 && x < BWIDTH)
                         board[y][x] = rand() % 7 + 1;
 
-                dead_time--;
-
-                if(dead_time == 0)
+                if(--dead_time == 0)
                         new_game();
         }
 
@@ -212,18 +188,11 @@ void new_game()
         falling_shape = 0;
 }
 
-//out of space?
-void game_over()
-{
-        dead_time = BWIDTH * BHEIGHT + EXTRA_DEAD;
-        next_shape = 0;
-}
-
-//randomly pick a new falling piece
+//randomly pick a new next piece, and put the old on in play
 void new_piece()
 {
         falling_shape = next_shape;
-        next_shape = rand() % SHAPES + 1;
+        next_shape = rand() % 7 + 1; // 7 shapes
 }
 
 //move the falling piece left, right, or down
@@ -237,6 +206,7 @@ void move(int dx, int dy)
         else if(dy)
         {
                 bake();
+                falling_shape = 0;
         }
 
         if(dy) idle_time = 0;
@@ -269,7 +239,6 @@ int collide(int x, int y, int rot)
                 if(board[world_j][world_i])
                         return 1;
         }
-
         return 0;
 }
 
@@ -287,28 +256,21 @@ void bake()
                 if(world_i < 0 || world_i >= BWIDTH || world_j < 0 || world_j >= BHEIGHT)
                         continue;
 
-                if(board[world_j][world_i])
-                        game_over();
+                if(board[world_j][world_i]) // already a block here? game over
+                {
+                        dead_time = BWIDTH * BHEIGHT;
+                        next_shape = 0;
+                }
 
                 board[world_j][world_i] = falling_shape;
         }
 
-        check_lines();
-        falling_shape = 0;
-}
-
-//check if there are any completed horizontal lines
-void check_lines()
-{
+        //check if there are any completed horizontal lines
         for(int j = BHEIGHT - 1; j >= 0; j--)
         {
-                for(int i = 0; i < BWIDTH; i++)
+                for(int i = 0; i < BWIDTH && board[j][i]; i++)
                 {
-                        if(!board[j][i])
-                                break;
-
-                        if(i == BWIDTH - 1)
-                                shine_line(j);
+                        if(i == BWIDTH - 1) shine_line(j);
                 }
         }
 }
@@ -319,7 +281,7 @@ void shine_line(int y)
         shine_time = 50;
         killy_lines[y] = 1;
         for(int i = 0; i < BWIDTH; i++)
-                board[y][i] = 8;
+                board[y][i] = 8; //shiny!
 }
 
 //remove lines that were marked to be removed by shine_line()
@@ -331,8 +293,8 @@ void kill_lines()
                 if(!killy_lines[y])
                         continue;
 
+                lines++;
                 new_lines++;
-
                 killy_lines[y] = 0;
                 memset(board[0], 0, sizeof *board);
 
@@ -342,8 +304,6 @@ void kill_lines()
                                 board[j][i] = board[j-1][i];
                 }
         }
-
-        lines += new_lines;
 
         switch(new_lines)
         {
@@ -357,12 +317,8 @@ void kill_lines()
 //move the falling piece as far down as it will go
 void slam()
 {
-        while(!collide(falling_x, falling_y + 1, falling_rot))
-        {
-                falling_y++;
+        for(; !collide(falling_x, falling_y + 1, falling_rot); falling_y++)
                 idle_time = 0;
-        }
-
 }
 
 //spin the falling piece left or right, if possible
@@ -379,34 +335,27 @@ void spin(int dir)
                 falling_x -= 1;
                 falling_rot = new_rot;
         }
-
 }
 
 //draw everything in the game on the screen
 void draw_stuff()
 {
+        // draw background, black boxes
         SDL_SetRenderDrawColor(renderer, 25, 40, 35, 255);
         SDL_RenderClear(renderer);
-
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-
-        //draw board background
         SDL_RenderFillRect(renderer, &(SDL_Rect){10, 10, BS * BWIDTH, BS * BHEIGHT});
-
-        //draw next box background
         SDL_RenderFillRect(renderer, &(SDL_Rect){10 + BS * BWIDTH + 10, 10, BS * 5, BS * 5});
 
-        //draw falling piece
+        //draw falling piece & shadow
         for(int i = 0; i < 4; i++) for(int j = 0; j < 4; j++)
         {
                 int world_i = i + falling_x;
                 int world_j = j + falling_y;
+                int shadow_j = MAX(world_j + 1, 0);
 
                 if(!is_solid_part(falling_shape, falling_rot, i, j))
                         continue;
-
-                int shadow_j = world_j + 1;
-                if(shadow_j < 0) shadow_j = 0;
 
                 SDL_SetRenderDrawColor(renderer, 8, 13, 12, 255);
                 SDL_RenderFillRect(renderer, &(SDL_Rect){
@@ -416,56 +365,37 @@ void draw_stuff()
                         BS * (BHEIGHT - shadow_j)
                 });
 
-                if(world_j < 0)
-                        continue;
-
-                draw_board_square(world_i, world_j, falling_shape);
+                if(world_j >= 0)
+                        draw_square(10 + BS * world_i, 10 + BS * world_j, falling_shape);
         }
 
-        //draw next piece 1) compute center of piece
-        int xmin = 3, xmax = 0, ymin = 3, ymax = 0;
+        //draw next piece, centered in the preview box
         for(int i = 0; i < 4; i++) for(int j = 0; j < 4; j++)
         {
                 if(is_solid_part(next_shape, 0, i, j))
-                {
-                        if(i < xmin) xmin = i;
-                        if(i > xmax) xmax = i;
-                        if(j < ymin) ymin = j;
-                        if(j > ymax) ymax = j;
-                }
-        }
-        int xdiff = (3 - xmax) - xmin;
-        int ydiff = (3 - ymax) - ymin;
-
-        //draw next piece 2) actually draw
-        for(int i = 0; i < 4; i++) for(int j = 0; j < 4; j++)
-        {
-                if(!is_solid_part(next_shape, 0, i, j))
-                        continue;
-
-                draw_square(
-                        10 + BS * BWIDTH + 10 + BS/2 + BS * i + (BS/2) * xdiff,
-                        10 + BS/2 + BS * j + (BS/2) * ydiff,
-                        next_shape
-                );
+                        draw_square(
+                                PREVIEW_BOX_X + BS * i + BS2 * center[2*next_shape],
+                                10 + BS * j + BS2 * center[2*next_shape + 1],
+                                next_shape
+                        );
         }
 
         //draw board pieces
         for(int i = 0; i < BWIDTH; i++) for(int j = 0; j < BHEIGHT; j++)
         {
-                if(!board[j][i])
-                        continue;
-
-                draw_board_square(i, j, board[j][i]);
+                if(board[j][i])
+                        draw_square(10 + BS * i, 10 + BS * j, board[j][i]);
         }
 
-        //draw text
-        text("Lines:", lines, 10 + BS * BWIDTH + 10, 10 + BS * 5 + 10 +   0);
-        text("%d"    , lines, 10 + BS * BWIDTH + 10, 10 + BS * 5 + 10 +  30);
-        text("Score:", score, 10 + BS * BWIDTH + 10, 10 + BS * 5 + 10 +  70);
-        text("%d"    , score, 10 + BS * BWIDTH + 10, 10 + BS * 5 + 10 + 100);
-        text("Best:" , best , 10 + BS * BWIDTH + 10, 10 + BS * 5 + 10 + 140);
-        text("%d"    , best , 10 + BS * BWIDTH + 10, 10 + BS * 5 + 10 + 170);
+        //draw counters and instructions
+        text("Lines:",       0, 10 + BS * BWIDTH + 10, 10 + BS * 5 + 10 +   0);
+        text("%d"    ,   lines, 10 + BS * BWIDTH + 10, 10 + BS * 5 + 10 +  30);
+        text("Score:",       0, 10 + BS * BWIDTH + 10, 10 + BS * 5 + 10 +  70);
+        text("%d"    ,   score, 10 + BS * BWIDTH + 10, 10 + BS * 5 + 10 + 100);
+        text("Best:" ,       0, 10 + BS * BWIDTH + 10, 10 + BS * 5 + 10 + 140);
+        text("%d"    ,    best, 10 + BS * BWIDTH + 10, 10 + BS * 5 + 10 + 170);
+        text("Controls:",    0, 10 + BS * BWIDTH + 10, 10 + BS * 5 + 10 + 370);
+        text("arrows, z, x", 0, 10 + BS * BWIDTH + 10, 10 + BS * 5 + 10 + 400);
 
         SDL_RenderPresent(renderer);
 }
@@ -473,35 +403,22 @@ void draw_stuff()
 //draw a single square/piece of a shape
 void draw_square(int x, int y, int shape)
 {
-        set_shape_color(shape, -25);
+        set_color_from_shape(shape, -25);
         SDL_RenderDrawRect(renderer, &(SDL_Rect){x, y, BS, BS});
-
-        set_shape_color(shape, 0);
+        set_color_from_shape(shape, 0);
         SDL_RenderFillRect(renderer, &(SDL_Rect){1 + x, 1 + y, BS - 2, BS - 2});
 }
 
-void draw_board_square(int bx, int by, int shape)
-{
-        draw_square(10 + BS * bx, 10 + BS * by, shape);
-}
-
 //set the current draw color to the color assoc. with a shape
-void set_shape_color(int shape, int shade)
+void set_color_from_shape(int shape, int shade)
 {
-        int r = colors[palette][shape*3 + 0] + shade;
-        int g = colors[palette][shape*3 + 1] + shade;
-        int b = colors[palette][shape*3 + 2] + shade;
-
-        if(r > 255) r = 255;
-        if(r <   0) r =   0;
-        if(g > 255) g = 255;
-        if(g <   0) g =   0;
-        if(b > 255) b = 255;
-        if(b <   0) b =   0;
-
+        int r = MAX(colors[shape*3 + 0] + shade, 0);
+        int g = MAX(colors[shape*3 + 1] + shade, 0);
+        int b = MAX(colors[shape*3 + 2] + shade, 0);
         SDL_SetRenderDrawColor(renderer, r, g, b, 255);
 }
 
+//render a centered line of text optionally with a %d value in it
 void text(char *fstr, int value, int x, int y)
 {
         if(!font) return;
@@ -509,7 +426,7 @@ void text(char *fstr, int value, int x, int y)
         char msg[80];
         snprintf(msg, 80, fstr, value);
         TTF_SizeText(font, msg, &w, &h);
-        SDL_Surface *msgsurf = TTF_RenderText_Blended(font, msg, (SDL_Color){0, 0, 0});
+        SDL_Surface *msgsurf = TTF_RenderText_Blended(font, msg, (SDL_Color){80, 90, 85});
         SDL_Texture *msgtex = SDL_CreateTextureFromSurface(renderer, msgsurf);
         SDL_Rect fromrec = {0, 0, msgsurf->w, msgsurf->h};
         SDL_Rect torec = {x, y, msgsurf->w, msgsurf->h};
