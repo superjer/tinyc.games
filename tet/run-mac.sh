411707012a9946c52028166d5a1d42271ad78906
Jer Wilson <jwilson@classifiedads.com>
2016-11-03 14:56:49 -0700

    Typo in run-mac.sh for Tet

--- tet/run-mac.sh
+++ tet/run-mac.sh
@@ -1,3 +1,3 @@
 export DYLD_FRAMEWORK_PATH=../_mac
 
-cc -I../_mac/SDL2.framework/Headers -I../_mac/SDL2_ttf.framework/Headers -F../_mac -framework SDL2 -framework SDL2_ttf -o tet tet && ./tet
+cc -I../_mac/SDL2.framework/Headers -I../_mac/SDL2_ttf.framework/Headers -F../_mac -framework SDL2 -framework SDL2_ttf -o tet tet.c && ./tet
