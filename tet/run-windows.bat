4f019408bd3a0e3b4b18d8aec4359b59d5011bf9
Jer Wilson <realaddress@legiturl.com>
2016-11-03 17:50:57 -0700

    Win32 compatibility

--- tet/run-windows.bat
+++ tet/run-windows.bat
@@ -1,12 +1,15 @@
 @echo off
 setlocal
 
-set CC=..\_win\tcc\tcc.exe
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && (set ARCH=x86) || (set ARCH=x64)
+
+set CC=..\_win\tcc-%ARCH%\tcc.exe
 set SDL=..\_win\SDL2-2.0.5
-set TTF=..\_win\SDL2_ttf-2.0.14\x86_64-w64-mingw32
+set TTF=..\_win\SDL2_ttf-2.0.14
 
-set PATH=%PATH%;%SDL%\lib\x64
-set PATH=%PATH%;%TTF%\lib
-set PATH=%PATH%;%TTF%\bin
+echo %CC%
 
-%CC% -DSDL_MAIN_HANDLED -I%SDL%\include -I%TTF%/include/SDL2 -L%SDL%\bin\x64 -L%TTF%/bin -lSDL2 -lSDL2_ttf -run tet.c
+set PATH=%PATH%;%SDL%\lib\%ARCH%
+set PATH=%PATH%;%TTF%\lib\%ARCH%
+
+%CC% -DSDL_MAIN_HANDLED -I%SDL%\include -I%TTF%\include -L%SDL%\lib\%ARCH% -L%TTF%\lib\%ARCH% -lSDL2 -lSDL2_ttf -run tet.c
