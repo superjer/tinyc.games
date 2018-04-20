0afb156b9c2529e69b1ad13a159a18f5ecbcf297
Jer Wilson <realaddress@legiturl.com>
2018-04-19 22:58:40 -0700

    switching to stb_image from sdl_image

--- blocko/run-linux.sh
+++ blocko/run-linux.sh
@@ -1,1 +1,1 @@
-tcc -g -Wall -Wextra -I/usr/include/GL/glew.h -I/usr/include/SDL2 -lGLEW -lSDL2 -lSDL2_ttf -lSDL2_image -run blocko.c
+tcc -g -Wall -Wextra -I/usr/include/GL/glew.h -I/usr/include/SDL2 -lGLEW -lSDL2 -run blocko.c
